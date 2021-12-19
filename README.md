a3a5360f8d81b02dbed340a4d5b53315c526fe4d
myako10s <myako10s@gmail.com>
2021-12-20 00:24:14 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # dotfiles
+
+## 1. Git Clone
+
+Clone this repository into `~/dotfiles`
+
+```sh
+git clone https://github.com/myako10s/dotfiles.git ~/dotfiles
+```
+
+## 2. Deploy dotfiles
+
+```sh
+# Do not override existing dotfiles
+bash ~/dotfiles/setup.sh deploy
+
+# Force override existing dotfiles
+bash ~/dotfiles/setup.sh -f deploy
+```
---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+OS="$(uname -s)"
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_TARBALL="https://github.com/myako10s/dotfiles/tarball/master"
+REMOTE_URL="https://github.com/myako10s/dotfiles.git"
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+usage() {
+  name=`basename $0`
+  cat <<EOF
+Usage:
+  $name [arguments] [command]
+Commands:
+  deploy
+Arguments:
+  -f $(tput setaf 1)** warning **$(tput sgr0) Overwrite dotfiles.
+  -h Print help (this message)
+EOF
+  exit 1
+}
+
+while getopts "fh" opt; do
+  case ${opt} in
+    f)
+      OVERWRITE=true
+      ;;
+    h)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# If missing, download and extract the dotfiles repository
+if [ ! -d ${DOT_DIRECTORY} ]; then
+  echo "Downloading dotfiles..."
+  mkdir ${DOT_DIRECTORY}
+
+  if has "git"; then
+    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
+  else
+    curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
+    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
+    rm -f ${HOME}/dotfiles.tar.gz
+  fi
+
+  echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+fi
+
+cd ${DOT_DIRECTORY}
+
+link_files() {
+  for f in .??*
+  do
+    # If you have ignore files, add file/directory name here
+    [[ ${f} = ".git" ]] && continue
+    #[[ ${f} = ".gitignore" ]] && continue
+
+    # Force remove the vim directory if it's already there
+    [ -n "${OVERWRITE}" -a -e ${HOME}/${f} ] && rm -f ${HOME}/${f}
+    if [ ! -e ${HOME}/${f} ]; then
+
+      ln -sfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+    fi
+  done
+
+  echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+}
+
+command=$1
+[ $# -gt 0 ] && shift
+
+case $command in
+  deploy)
+    link_files
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+exit 0
