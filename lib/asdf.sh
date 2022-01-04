e76470602a887298bfb9b19f87ce52b72aa7f6ef
myako10s <myako10s@gmail.com>
2022-01-04 16:21:37 +0900

    fix misspelled

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -12,13 +12,16 @@ run_asdf() {
   else
     echo "Installing asdf..."
     brew install asdf
-    echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.bash_profile
-    echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.bash_profile
+    if ! grep -q "$(brew --prefix asdf)/libexec/asdf.sh" ~/.bash_profile; then
+      echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ~/.bash_profile
+      echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.bash_profile
+    fi
   fi
 
   if has "asdf"; then
     if [ -e ${HOME}/.tool-versions ]; then
       echo "Installing asdf packages..."
+      # avoid macos-ld-symbols-not-found-for-architecture-x86_64-1245 issue.
       path_remove $(brew --prefix binutils)/bin
       asdf install
       [[ $? ]] && echo "$(tput setaf 2)Install packages complete. ✔︎$(tput sgr0)"
