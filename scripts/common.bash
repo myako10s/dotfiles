c932d4000707cd5ff0a0eb50a6360058a4ff7c68
myako10s <19725261+myako10s@users.noreply.github.com>
2025-09-13 21:53:30 +0900

    Add new scripts

---
+++ scripts/common.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+
+export SCRIPTS DOTFILES
+SCRIPTS="$(cd "$(dirname "$0")" || exit 1; pwd)"
+DOTFILES="$(cd "$(dirname "$0")/.." || exit 1; pwd)"
+
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
