beff593c4dbc6a99e5b0b6c30b2616b837b68a91
myako10s <19725261+myako10s@users.noreply.github.com>
2026-07-24 00:30:57 +0900

    feat: add yazi function for improved directory navigation

--- config/zsh/lazy.zsh
+++ config/zsh/lazy.zsh
@@ -131,6 +131,16 @@ if has ghq fzf bat; then
     bindkey '^]' fzf-ghq-widget
 fi
 
+## yazi
+if has yazi; then
+    function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        command yazi "$@" --cwd-file="$tmp"
+        IFS= read -r -d '' cwd < "$tmp"
+        [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+        command rm -f -- "$tmp"
+    }
+fi
 
 ## dotfiles management
 # run all setup scripts
